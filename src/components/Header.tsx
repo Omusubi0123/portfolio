a224f6e5b7e7070d6868eb4f67dfc311f3cdd12e
Omusubi0123 <Vermouth77rseidl@gmail.com>
2025-01-06 21:41:07 +0900

    fix: デプロイページでHeaderが半透明にならないバグ解消

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   const sections = ['About', 'Works', 'Skills', 'Certifications'];
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-gray-800 text-white shadow-md z-10 h-10 backdrop-blur-sm bg-black/60 rounded-lg">
+    <header className="fixed top-0 left-0 w-full text-white shadow-md z-10 h-10 backdrop-blur-sm bg-black/60 rounded-lg">
       <nav className="max-w-screen-lg mx-auto flex justify-between items-center px-20 py-4 h-full">
         <h1 className="text-2xl font-bold">My Portfolio</h1>
         <ul className="flex space-x-8">
